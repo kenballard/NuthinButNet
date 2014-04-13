1ff3ef21215c0eb2b71d01cd096dd85aace4a4e0
Ben Liset <bliset2@gmail.com>
2014-04-12 19:40:39 -0500

    Blog Listings now page

--- NuthinButNet/NuthinButNet/Controllers/BlogController.cs
+++ NuthinButNet/NuthinButNet/Controllers/BlogController.cs
@@ -12,22 +12,29 @@ namespace NuthinButNet.Controllers
     {
         [HttpGet]
         [ChildActionOnly]
-        public ActionResult Index(int? page)
+        public ActionResult Listings(int? page)
         {
-            int pageNumber = page ?? 0;
+            int pageNumber = page ?? 1;
             var model = new BlogLandingViewModel();
 
             int blogsPerPage = int.Parse(CurrentPage.GetProperty("articlesPerPage").Value.ToString());
-            int skipAmount = blogsPerPage * pageNumber;
+            int skipAmount = blogsPerPage * (pageNumber - 1);
 
             var blogPosts = CurrentPage.Children.OrderByDescending(x => DateTime.Parse(x.GetProperty("date").Value.ToString()));
             int totalBlogPosts = blogPosts.Count();
 
             model.BlogPosts = blogPosts.Skip(skipAmount).Take(blogsPerPage);
 
-            if (pageNumber > 0 && totalBlogPosts > blogsPerPage)
+            if (pageNumber > 1 && totalBlogPosts > blogsPerPage)
             {
-                model.PreviousButtonUrl = "?page=" + (pageNumber - 1);
+                if (pageNumber == 2)
+                {
+                    model.PreviousButtonUrl = ".";
+                }
+                else
+                {
+                    model.PreviousButtonUrl = "?page=" + (pageNumber - 1);
+                }               
             }
             if (totalBlogPosts > skipAmount + blogsPerPage)
             {
