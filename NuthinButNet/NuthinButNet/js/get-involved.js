56dfe740b43e75240990589ddc245b15fe1e1181
Ken Ballard <mail@kenballard.me>
2014-04-12 20:18:43 -0500

    Cleaning up catalog selections.

--- NuthinButNet/NuthinButNet/js/get-involved.js
+++ NuthinButNet/NuthinButNet/js/get-involved.js
@@ -1,19 +1,22 @@
 ﻿$(document).ready(function () {
     $('.addToCart').click(function () {
-        var selMoney = $('input[name=money]:checked', '#catalog').val();
-        var selTime = $('input[name=time]:checked', '#catalog').val();
-        selMoney = selMoney == '-1' ? $('#moneyOther').val() : selMoney;
-        selTime = selTime == '-1' ? $('#timeOther').val() : selTime;
-        
+        var selMoney = getSingleProduct('money');
+        var selTime = getSingleProduct('time');
         var selTalent = getMultiProducts('input[name=talent]:checked', '#catalog');
         var selInKind = getMultiProducts('input[name=inKind]:checked', '#catalog');
+
+        function getSingleProduct(selector, parentSelector, otherSelector)
+        {
+            var sel = $('input[name=' + selector + ']:checked', '#catalog').val();
+            sel = sel == '-1' ? $('#' + selector + 'Other').val() : selMoney;
+        }
 
         function getMultiProducts(selector)
         {
             var values = '';
             $(selector).each(function () {
                 if (values != '')
-                    values += ',';
+                    values += ', ';
                 values += $(this).val();
             });
             return values;
