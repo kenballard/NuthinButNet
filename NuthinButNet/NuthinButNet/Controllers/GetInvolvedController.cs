d6efaca5ea2776366e6a610f7088b8e94cdbe7e6
Ken Ballard <kballard@nerdery.com>
2014-04-12 19:36:00 -0500

    Capturing selected values in catalog.

---
+++ NuthinButNet/NuthinButNet/Controllers/GetInvolvedController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+
+namespace NuthinButNet.Controllers
+{
+    public class GetInvolvedController : SurfaceController
+    {
+
+    }
+}
--- NuthinButNet/NuthinButNet/js/get-involved.js
+++ NuthinButNet/NuthinButNet/js/get-involved.js
@@ -1,5 +1,23 @@
 ﻿$(document).ready(function () {
-    $('#addToCart').click(function () {
-        $("#moneySelected").text($("#money").val());
+    $('.addToCart').click(function () {
+        var selMoney = $('input[name=money]:checked', '#catalog').val();
+        var selTime = $('input[name=time]:checked', '#catalog').val();
+        selMoney = selMoney == '-1' ? $('#moneyOther').val() : selMoney;
+        selTime = selTime == '-1' ? $('#timeOther').val() : selTime;
+        
+        var selTalent = getMultiProducts('input[name=talent]:checked', '#catalog');
+        var selInKind = getMultiProducts('input[name=inKind]:checked', '#catalog');
+
+        function getMultiProducts(selector)
+        {
+            var values = '';
+            $(selector).each(function () {
+                if (values != '')
+                    values += ',';
+                values += $(this).val();
+            });
+            return values;
+        }
     });
+
 });
